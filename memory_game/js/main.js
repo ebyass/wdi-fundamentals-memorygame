467bfe4065f8e1a21641d5c120cbc496f4da6ec1
Eleanor <eleanorbyass@gmail.com>
2020-03-13 11:45:06 +0000

    Added JavaScript game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,56 @@
 console.log ("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardTwo);
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+function checkForMatch (){
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again");
+	}
+}
+function flipCard (cardId) {
+
+if (cardsInPlay.length === 2 ) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+
+		alert("You've found a match!"); }
+		 
+		 else {
+
+			alert("Sorry Try Again.");
+		}
+}
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	
+};
+
+
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
+
+
+//if cards.length === 2; cardsInPlay = cardsInPlay[0] === cardsInPlay[1] ? alert ("you've found a match!") : alert ("Sorry Try Again.");
+
+
+
+
+
+
+
